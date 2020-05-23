a048954aa4ea6e0f922335bc6070efbd3c6e1a7c
wat128 <wat128sideb@gmail.com>
2020-05-23 18:24:29 +0900

    No3 タッチイベントで画像を動かす

--- app/src/main/java/com/wat128/catchtheball/MainActivity.java
+++ app/src/main/java/com/wat128/catchtheball/MainActivity.java
@@ -1,14 +1,53 @@
 package com.wat128.catchtheball;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView scoreLabel;
+    private TextView startLabel;
+    private ImageView box;
+    private ImageView orange;
+    private ImageView pink;
+    private ImageView black;
+
+    // 位置
+    private float boxY;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN)
+            boxY -= 20;
+
+        box.setY(boxY);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scoreLabel = findViewById(R.id.scoreLabel);
+        startLabel = findViewById(R.id.startLabel);
+        box = findViewById(R.id.box);
+        orange = findViewById(R.id.orange);
+        pink = findViewById(R.id.pink);
+        black = findViewById(R.id.black);
+
+        orange.setX(-80.0f);
+        orange.setY(-80.0f);
+        pink.setX(-80.0f);
+        pink.setY(-80.0f);
+        black.setX(-80.0f);
+        black.setY(-80.0f);
+
+        startLabel.setVisibility(View.INVISIBLE);
+        boxY = 500.0f;
     }
 }
