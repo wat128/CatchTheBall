011a4cb1ad444c34d0281b49f54543fc2f8d8eac
wat128 <wat128sideb@gmail.com>
2020-05-26 16:54:53 +0900

    タイトル画面にハイスコアを表示する

--- app/src/main/java/com/wat128/catchtheball/StartActivity.java
+++ app/src/main/java/com/wat128/catchtheball/StartActivity.java
@@ -3,17 +3,25 @@ package com.wat128.catchtheball;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
 
     private SoundPlayer soundPlayer;
+    private TextView highScoreLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        highScoreLabel = findViewById(R.id.highScoreLabel);
+        SharedPreferences sharedPreferences = getSharedPreferences("GAME_DATA", MODE_PRIVATE);
+        int highScore = sharedPreferences.getInt("HIGH_SCORE", 0);
+        highScoreLabel.setText(highScore + "");
 
         soundPlayer = new SoundPlayer(this);
     }
